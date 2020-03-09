4e3b27df364bc0cabcbc705137f13d2b6664d96e
darono87 <darono87@gmail.com>
2020-03-09 10:36:32 +0100

    Google Map attached to the project

--- Frontend/tour_advisor/src/components/MainMap.vue
+++ Frontend/tour_advisor/src/components/MainMap.vue
@@ -1,16 +1,47 @@
 <template>
- <div>
-   Map Container
- </div>
+  <section>
+    <h3>Map Container</h3>
+    <div id = "map">
+      
+    </div>
+  </section>
+  
 </template>
 
 <script>
-
 export default {
-  name: 'MapContainer'
+  name: 'MapContainer',
+  mounted() {
+
+    let callbackName = "googleLoaded";
+    let googleMaps = document.createElement('script')
+    googleMaps.setAttribute('src', `https://maps.googleapis.com/maps/api/js?key=AIzaSyB_md0MiltqnpcDC18v1avPsS0zhDXsUj4&callback=${callbackName}`);
+    googleMaps.innerHTML = "google.maps.Map(document.getElementById('map'), {zoom: 4, center: {lat:0,long:0}});";
+    document.head.appendChild(googleMaps);
+
+    window[callbackName] = ()=>{
+      if(!google)
+        console.log("error");
+      const map = new google.maps.Map(document.querySelector("#map"),{
+          center: {lat: 0, lng: 0},
+          zoom: 3
+        });
+    }
+    
+
+  }, 
+  methods: {
+    showMap(){
+      console.log("test");
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+#map{
+  width: 800px;
+  height: 400px;
+  margin-bottom: 100px;
+}
 </style>
