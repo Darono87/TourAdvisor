7764ddeb3f91198f10eafa666f8ada3bf93e0677
darono87 <darono87@gmail.com>
2020-03-12 20:35:30 +0100

    User basic register

--- Backend/src/router.js
+++ Backend/src/router.js
@@ -1,12 +1,20 @@
 const express = require("express");
-
+const {user} = require('./models')
 const router = express.Router();
 
-router.post('/register', (req, res)=>{
-  res.send({
-    message: 'Backend has received your data. Your client email is: ' +
-    req.body.email,
-  });
+router.post('/register', async (req, res)=>{
+  try{
+    let newUser = await user.create(req.body);
+    res.send({
+      message: 'Backend has received your data. Your client email is: ' +
+      req.body.email,
+      userData: newUser
+    });
+  }catch(e){
+    res.status(400).send({
+      message: 'Something has gone wrong'
+    });
+  }
 });
 
 module.exports = router;
