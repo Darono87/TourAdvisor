32087fa930caa5f75383eaf20de1d8662033a813
darono87 <darono87@gmail.com>
2020-05-01 00:37:09 +0200

    Remembering login, forgets it when token expires, tokenTime in localStorage

--- Frontend/tour_advisor/src/store/index.js
+++ Frontend/tour_advisor/src/store/index.js
@@ -3,23 +3,42 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+function determineLoggedState(){
+  let timePassed = Date.now() - localStorage.getItem('tokenTime');
+  var returnObject = null;
+  if(timePassed >= 86400000){
+    returnObject = {
+      userToken: null,
+      nickname: null
+    }
+  }
+  else
+    returnObject = {
+      userToken: localStorage.getItem('userToken') || null,
+      nickname: localStorage.getItem('userNickname') || null
+    };
+  return returnObject;
+}
+
 export default new Vuex.Store({
   state: {
-    userToken: localStorage.getItem('userToken') || null,
-    nickname: localStorage.getItem('userNickname') || null
+    ...determineLoggedState()
   },
   mutations: {
     login (state, loginData){
+      
       state.userToken = loginData.token;
       localStorage.setItem('userToken',loginData.token);
       state.nickname = loginData.nickname;
       localStorage.setItem('userNickname',loginData.nickname);
+      localStorage.setItem('tokenTime',Date.now());
     },
     logout (state){
       state.userToken = null;
       localStorage.removeItem('userToken');
       state.nickname = null;
       localStorage.removeItem('userNickname');
+      localStorage.removeItem('tokenTime');
     }
   },
   actions: {
